f729093a0088b7611da61299c1aef7d3fe54a66d
stoakes <Stoakes@users.noreply.github.com>
2017-03-03 06:49:17 +0100

    Manage web folder relative link Beautify code

--- main.js
+++ main.js
@@ -5,32 +5,35 @@ const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
-var pdfIndex = process.argv.indexOf("--pdf")+1;
+var pdfIndex = process.argv.indexOf("--pdf") + 1;
 var devIndex = process.argv.indexOf("-dev");
 
 var mainWindow = null;
 
-const pdfURL = process.argv[pdfIndex];
+var pdfURL = process.argv[pdfIndex];
+// is it an absolute path or a relative one (in that case, add ./../../ to the path) ?
+if (pdfURL.charAt(0) != '/') {
+    pdfURL = './../../' + pdfURL;
+}
 
-app.on('ready', function() {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: false,
-      webSecurity: false,
-    },
-  });
+app.on('ready', function () {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: false,
+            webSecurity: false,
+        },
+    });
+    const param = qs.stringify({file: pdfURL});
 
-  const param = qs.stringify({file: pdfURL});
+    mainWindow.loadURL('file://' + __dirname + '/pdfjs/web/viewer.html?' + param);
 
-  mainWindow.loadURL('file://' + __dirname + '/pdfjs/web/viewer.html?' + param);
+    if (devIndex != -1) {
+        mainWindow.webContents.openDevTools();
+    }
 
-  if (devIndex != -1) {
-    mainWindow.webContents.openDevTools();
-  }
-
-  mainWindow.on('closed', function() {
-    mainWindow = null;
-  });
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
 });
